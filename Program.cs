3f1f612c57827e2271b1d3a3f22e681d6416cab8
Kate Sokolova <katunichkaa@mail.ru>
2022-08-12 00:00:17 +0300

    Добавлено введение длины массива N с клавиатуры, задан начальный  массив

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿Console.WriteLine("Введите количество элиментов массива");
+int N = Convert.ToInt32(Console.ReadLine());
+int i;
+Console.WriteLine("Mассив:");
+string [] array = new string [N];
+
+

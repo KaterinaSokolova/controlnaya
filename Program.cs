c609cf6d5bdc93b86badccdcb5aaefb9f7f50e40
Kate Sokolova <katunichkaa@mail.ru>
2022-08-12 00:14:33 +0300

    Отформатированна программа

--- Program.cs
+++ Program.cs
@@ -2,37 +2,43 @@
 int N = Convert.ToInt32(Console.ReadLine());
 int i;
 Console.WriteLine("Mассив:");
-string [] array = new string [N];
+string[] array = new string[N];
 
-  for(i=0; i<N; i++){
-        Console.WriteLine($"Введите элимент массива с индексом {i}");
-        array[i] = Console.ReadLine();        
+for (i = 0; i < N; i++)
+{
+    Console.WriteLine($"Введите элимент массива с индексом {i}");
+    array[i] = Console.ReadLine();
 }
 
 Console.Write("Mассив: [");
-for(i=0; i<N; i++){
-Console.Write(array[i] + " ");
+for (i = 0; i < N; i++)
+{
+    Console.Write(array[i] + " ");
 }
 Console.Write("]");
 
 int count = 0;
-for(i=0; i<N; i++){
-    if(array[i].Length<=3){
+for (i = 0; i < N; i++)
+{
+    if (array[i].Length <= 3)
+    {
         count++;
     }
 }
 Console.WriteLine();
 
-string [] array2 = new string [count];
+string[] array2 = new string[count];
 
 int j = 0;
 Console.Write("Новый массив: [");
-for(i=0; i<N;i++){
-    if(array[i].Length<=3){
+for (i = 0; i < N; i++)
+{
+    if (array[i].Length <= 3)
+    {
         array2[j] = array[i];
         Console.Write(array2[j] + " ");
         j++;
-            }
+    }
 
 }
 Console.Write("]");
